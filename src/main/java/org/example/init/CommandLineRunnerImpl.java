bff7879738f35c3f5d919557d536def7b7fb5fdd
PCAdmin <mkhoyanAr@gmail.com>
2023-12-04 19:51:33 +0300

    fixed the design

--- src/main/java/org/example/init/CommandLineRunnerImpl.java
+++ src/main/java/org/example/init/CommandLineRunnerImpl.java
@@ -101,27 +101,27 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
 
         OfferDTO offerDTO1 = offerService.createOffer(new OfferDTO("Все автомобили, представленные в продаже, проходят диагностику по всем параметрам, с результатами диагностики Вы можете ознакомиться при осмотре автомобиля."
                 , EngineType.GASOLINE, "/images/offers/ToyotaCamry2019.png", 81341, new BigDecimal("2399000.00"), TransmissionType.AUTOMATIC, 2019, model1, user1));
-        OfferDTO offerDTO2 = offerService.createOffer(new OfferDTO("Распродажа, финальный sale!\n\uD83D\uDCA5Гарантия 2 ГОДА или 100 000км пробега на ВСЕ НОВЫЕ автомобили BMW в РОЛЬФ Премиум Вешки!\uD83D\uDCA5"
-                , EngineType.DIESEL, "/images/offers/BMWX52023.png", 0, new BigDecimal("15850000.00"), TransmissionType.AUTOMATIC, 2023, model2, user2));
-        OfferDTO offerDTO3 = offerService.createOffer(new OfferDTO("Обслуживалась у официалов.\nПробег родной. Один хозяин.\nВ наличии комплект летней резины на дисках. Сигнализация с автозапуском."
-                , EngineType.GASOLINE, "/images/offers/ToyotaCorolla2013.png", 101000, new BigDecimal("1500000.00"), TransmissionType.MANUAL, 2013, model3, admin));
-        OfferDTO offerDTO4 = offerService.createOffer(new OfferDTO("Description4", EngineType.GASOLINE, "offerImageUrl4", 55000, new BigDecimal("10500.00"), TransmissionType.AUTOMATIC, 2017, model4, user3));
-        OfferDTO offerDTO5 = offerService.createOffer(new OfferDTO("Description5", EngineType.DIESEL, "offerImageUrl5", 62000, new BigDecimal("15500.00"), TransmissionType.MANUAL, 2018, model5, user4));
-        OfferDTO offerDTO6 = offerService.createOffer(new OfferDTO("Description6", EngineType.GASOLINE, "offerImageUrl6", 59000, new BigDecimal("11000.00"), TransmissionType.AUTOMATIC, 2016, model6, user5));
-        OfferDTO offerDTO7 = offerService.createOffer(new OfferDTO("Description7", EngineType.DIESEL, "offerImageUrl7", 68000, new BigDecimal("16000.00"), TransmissionType.MANUAL, 2015, model7, user6));
-        OfferDTO offerDTO8 = offerService.createOffer(new OfferDTO("Description8", EngineType.GASOLINE, "offerImageUrl8", 51000, new BigDecimal("10000.00"), TransmissionType.AUTOMATIC, 2013, model8, user7));
-        OfferDTO offerDTO9 = offerService.createOffer(new OfferDTO("Description9", EngineType.DIESEL, "offerImageUrl9", 63000, new BigDecimal("15000.00"), TransmissionType.MANUAL, 2012, model9, user8));
-        OfferDTO offerDTO10 = offerService.createOffer(new OfferDTO("Description10", EngineType.GASOLINE, "offerImageUrl10", 57000, new BigDecimal("11500.00"), TransmissionType.AUTOMATIC, 2010, model10, user9));
-        OfferDTO offerDTO11 = offerService.createOffer(new OfferDTO("Description11", EngineType.DIESEL, "offerImageUrl11", 61000, new BigDecimal("15500.00"), TransmissionType.MANUAL, 2008, model11, user10));
-        OfferDTO offerDTO12 = offerService.createOffer(new OfferDTO("Description12", EngineType.GASOLINE, "offerImageUrl12", 54000, new BigDecimal("10000.00"), TransmissionType.AUTOMATIC, 2006, model12, user11));
-        OfferDTO offerDTO13 = offerService.createOffer(new OfferDTO("Description13", EngineType.DIESEL, "offerImageUrl13", 67000, new BigDecimal("16000.00"), TransmissionType.MANUAL, 2004, model13, user12));
-        OfferDTO offerDTO14 = offerService.createOffer(new OfferDTO("Description14", EngineType.GASOLINE, "offerImageUrl14", 52000, new BigDecimal("10500.00"), TransmissionType.AUTOMATIC, 2002, model14, user13));
-        OfferDTO offerDTO15 = offerService.createOffer(new OfferDTO("Description15", EngineType.DIESEL, "offerImageUrl15", 65000, new BigDecimal("15500.00"), TransmissionType.MANUAL, 2000, model15, user14));
-        OfferDTO offerDTO16 = offerService.createOffer(new OfferDTO("Description16", EngineType.GASOLINE, "offerImageUrl16", 53000, new BigDecimal("11000.00"), TransmissionType.AUTOMATIC, 1998, model16, user15));
-        OfferDTO offerDTO17 = offerService.createOffer(new OfferDTO("Description17", EngineType.DIESEL, "offerImageUrl17", 66000, new BigDecimal("16500.00"), TransmissionType.MANUAL, 1996, model17, user16));
-        OfferDTO offerDTO18 = offerService.createOffer(new OfferDTO("Description18", EngineType.GASOLINE, "offerImageUrl18", 56000, new BigDecimal("10500.00"), TransmissionType.AUTOMATIC, 1994, model18, user17));
-        OfferDTO offerDTO19 = offerService.createOffer(new OfferDTO("Description19", EngineType.DIESEL, "offerImageUrl19", 64000, new BigDecimal("15500.00"), TransmissionType.MANUAL, 1992, model19, user18));
-        OfferDTO offerDTO20 = offerService.createOffer(new OfferDTO("Description20", EngineType.GASOLINE, "offerImageUrl20", 55000, new BigDecimal("10000.00"), TransmissionType.AUTOMATIC, 1990, model20, admin));
+//        OfferDTO offerDTO2 = offerService.createOffer(new OfferDTO("Распродажа, финальный sale!\n\uD83D\uDCA5Гарантия 2 ГОДА или 100 000км пробега на ВСЕ НОВЫЕ автомобили BMW в РОЛЬФ Премиум Вешки!\uD83D\uDCA5"
+//                , EngineType.DIESEL, "/images/offers/BMWX52023.png", 0, new BigDecimal("15850000.00"), TransmissionType.AUTOMATIC, 2023, model2, user2));
+//        OfferDTO offerDTO3 = offerService.createOffer(new OfferDTO("Обслуживалась у официалов.\nПробег родной. Один хозяин.\nВ наличии комплект летней резины на дисках. Сигнализация с автозапуском."
+//                , EngineType.GASOLINE, "/images/offers/ToyotaCorolla2013.png", 101000, new BigDecimal("1500000.00"), TransmissionType.MANUAL, 2013, model3, admin));
+//        OfferDTO offerDTO4 = offerService.createOffer(new OfferDTO("Description4", EngineType.GASOLINE, "offerImageUrl4", 55000, new BigDecimal("10500.00"), TransmissionType.AUTOMATIC, 2017, model4, user3));
+//        OfferDTO offerDTO5 = offerService.createOffer(new OfferDTO("Description5", EngineType.DIESEL, "offerImageUrl5", 62000, new BigDecimal("15500.00"), TransmissionType.MANUAL, 2018, model5, user4));
+//        OfferDTO offerDTO6 = offerService.createOffer(new OfferDTO("Description6", EngineType.GASOLINE, "offerImageUrl6", 59000, new BigDecimal("11000.00"), TransmissionType.AUTOMATIC, 2016, model6, user5));
+//        OfferDTO offerDTO7 = offerService.createOffer(new OfferDTO("Description7", EngineType.DIESEL, "offerImageUrl7", 68000, new BigDecimal("16000.00"), TransmissionType.MANUAL, 2015, model7, user6));
+//        OfferDTO offerDTO8 = offerService.createOffer(new OfferDTO("Description8", EngineType.GASOLINE, "offerImageUrl8", 51000, new BigDecimal("10000.00"), TransmissionType.AUTOMATIC, 2013, model8, user7));
+//        OfferDTO offerDTO9 = offerService.createOffer(new OfferDTO("Description9", EngineType.DIESEL, "offerImageUrl9", 63000, new BigDecimal("15000.00"), TransmissionType.MANUAL, 2012, model9, user8));
+//        OfferDTO offerDTO10 = offerService.createOffer(new OfferDTO("Description10", EngineType.GASOLINE, "offerImageUrl10", 57000, new BigDecimal("11500.00"), TransmissionType.AUTOMATIC, 2010, model10, user9));
+//        OfferDTO offerDTO11 = offerService.createOffer(new OfferDTO("Description11", EngineType.DIESEL, "offerImageUrl11", 61000, new BigDecimal("15500.00"), TransmissionType.MANUAL, 2008, model11, user10));
+//        OfferDTO offerDTO12 = offerService.createOffer(new OfferDTO("Description12", EngineType.GASOLINE, "offerImageUrl12", 54000, new BigDecimal("10000.00"), TransmissionType.AUTOMATIC, 2006, model12, user11));
+//        OfferDTO offerDTO13 = offerService.createOffer(new OfferDTO("Description13", EngineType.DIESEL, "offerImageUrl13", 67000, new BigDecimal("16000.00"), TransmissionType.MANUAL, 2004, model13, user12));
+//        OfferDTO offerDTO14 = offerService.createOffer(new OfferDTO("Description14", EngineType.GASOLINE, "offerImageUrl14", 52000, new BigDecimal("10500.00"), TransmissionType.AUTOMATIC, 2002, model14, user13));
+//        OfferDTO offerDTO15 = offerService.createOffer(new OfferDTO("Description15", EngineType.DIESEL, "offerImageUrl15", 65000, new BigDecimal("15500.00"), TransmissionType.MANUAL, 2000, model15, user14));
+//        OfferDTO offerDTO16 = offerService.createOffer(new OfferDTO("Description16", EngineType.GASOLINE, "offerImageUrl16", 53000, new BigDecimal("11000.00"), TransmissionType.AUTOMATIC, 1998, model16, user15));
+//        OfferDTO offerDTO17 = offerService.createOffer(new OfferDTO("Description17", EngineType.DIESEL, "offerImageUrl17", 66000, new BigDecimal("16500.00"), TransmissionType.MANUAL, 1996, model17, user16));
+//        OfferDTO offerDTO18 = offerService.createOffer(new OfferDTO("Description18", EngineType.GASOLINE, "offerImageUrl18", 56000, new BigDecimal("10500.00"), TransmissionType.AUTOMATIC, 1994, model18, user17));
+//        OfferDTO offerDTO19 = offerService.createOffer(new OfferDTO("Description19", EngineType.DIESEL, "offerImageUrl19", 64000, new BigDecimal("15500.00"), TransmissionType.MANUAL, 1992, model19, user18));
+//        OfferDTO offerDTO20 = offerService.createOffer(new OfferDTO("Description20", EngineType.GASOLINE, "offerImageUrl20", 55000, new BigDecimal("10000.00"), TransmissionType.AUTOMATIC, 1990, model20, admin));
 
     }
 
